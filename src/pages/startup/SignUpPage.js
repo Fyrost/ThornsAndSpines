35888f789485a9c49cc3cd05a619047188e5fec6
fyrost <fmga.webbie@gmail.com>
2020-02-17 11:20:43 +0800

    [:sparkles:] added and styled text input

--- src/pages/startup/SignUpPage.js
+++ src/pages/startup/SignUpPage.js
@@ -1,28 +1,50 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Button } from "react-native-elements";
+import { View,Text } from "react-native";
+import { Button, Input } from "react-native-elements";
 
 class SignUpPage extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>SignUp</Text>
-        <Button
+        <Text>Customer Information</Text>
+        <Input
+          label="First Name:*"
+          labelStyle={styles.inputLabel}
+          inputStyle={styles.inputText}
+          inputContainerStyle={styles.inputContainer}
+        />
+        {/* <Button
           title={"Login"}
           onPress={() => this.props.navigation.navigate("Login")}
-        />
+        /> */}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  inputLabel: {
+    color: "#2d6a27",
+    fontWeight: "normal",
+    fontSize: 13
+  },
+  inputText: {
+    fontSize: 13
+  },
+  inputContainer: {
+    borderColor: "#2d6a27",
+    borderWidth: 1,
+    borderRadius: 7,
+    height: 30,
+    paddingHorizontal: 5
   }
-});
+};
 
 export default SignUpPage;
