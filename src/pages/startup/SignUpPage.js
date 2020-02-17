e4577464645cc8495c2cc352b2b8a339a7d179a1
fyrost <fmga.webbie@gmail.com>
2020-02-17 11:51:52 +0800

    [:sparkles:] implemented focus shifting

--- src/pages/startup/SignUpPage.js
+++ src/pages/startup/SignUpPage.js
@@ -1,23 +1,78 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { Button, Input } from "react-native-elements";
+import KeyboardShift from "../../component/KeyboardShift";
 
 class SignUpPage extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Customer Information</Text>
-        <Input
-          label="First Name:*"
-          labelStyle={styles.inputLabel}
-          inputStyle={styles.inputText}
-          inputContainerStyle={styles.inputContainer}
-        />
-        {/* <Button
+      <ScrollView>
+        <KeyboardShift style={{ flex: 1 }}>
+          <View style={styles.container}>
+            <Text style={styles.titleText}>Customer Information</Text>
+            <Input
+              label="First Name:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="Last Name:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="Address:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="City:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="Email:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="Password:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Text style={styles.titleText}>Shipping Information</Text>
+            <Input
+              label="Shipping Address:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="City:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+            <Input
+              label="Region:*"
+              labelStyle={styles.inputLabel}
+              inputStyle={styles.inputText}
+              inputContainerStyle={styles.inputContainer}
+            />
+
+            {/* <Button
           title={"Login"}
           onPress={() => this.props.navigation.navigate("Login")}
         /> */}
-      </View>
+          </View>
+        </KeyboardShift>
+      </ScrollView>
     );
   }
 }
@@ -39,7 +94,7 @@ const styles = {
     color: "#2d6a27",
     fontWeight: "normal",
     fontSize: 13,
-    marginBottom:3
+    marginBottom: 3
   },
   inputText: {
     fontSize: 13
@@ -49,7 +104,8 @@ const styles = {
     borderWidth: 1,
     borderRadius: 7,
     height: 30,
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
+    marginBottom: 3
   }
 };
 
