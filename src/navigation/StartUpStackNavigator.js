38db67a0ebfdd43706173c909878e467ea435ce0
fyrost <fmga.webbie@gmail.com>
2020-02-18 16:58:31 +0800

    [:art:] added resend and verify navigation

--- src/navigation/StartUpStackNavigator.js
+++ src/navigation/StartUpStackNavigator.js
@@ -1,26 +1,42 @@
 import { createStackNavigator } from "react-navigation";
 import LoginPage from "../pages/startup/LoginPage";
 import SignUpPage from "../pages/startup/SignUpPage";
-
-export default createStackNavigator({
-  Login: {
-    screen: LoginPage,
-    navigationOptions: {
-      header: null
+import ResendPage from "../pages/startup/ResendPage";
+import VerifyPage from "../pages/startup/VerifyPage";
+export default createStackNavigator(
+  {
+    Login: {
+      screen: LoginPage,
+      navigationOptions: {
+        header: null
+      }
+    },
+    SignUp: {
+      screen: SignUpPage,
+      navigationOptions: {
+        title: "Create Account"
+      }
+    },
+    Resend: {
+      screen: ResendPage,
+      navigationOptions: {
+        title: "Resend Verification Code"
+      }
+    },
+    Verify: {
+      screen: VerifyPage,
+      navigationOptions: {
+        title: "Verify"
+      }
     }
   },
-  SignUp: {
-    screen: SignUpPage,
-    navigationOptions: {
-      title: "Create Account",
+  {
+    headerLayoutPreset: "center",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#2d6a27"
+      },
+      headerTintColor: "white"
     }
   }
-},{
-  headerLayoutPreset:"center",
-  defaultNavigationOptions:{
-    headerStyle:{
-      backgroundColor:"#2d6a27"
-    },
-    headerTintColor: "white"
-  }
-});
+);
