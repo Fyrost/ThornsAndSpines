cede0dcd6974c5b948296a7a61926d9d8b15825b
fyrost <fmga.webbie@gmail.com>
2020-03-02 11:03:46 +0800

    [:hammer_and_wrench:] cart style and functionality

--- src/pages/app/cart/CartPage.js
+++ src/pages/app/cart/CartPage.js
@@ -1,16 +1,82 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
 import { Button } from "react-native-elements";
+import CartList from "../../../component/CartList";
+import { getCart, catchError } from "../../../misc/api";
+import { NavigationEvents } from "react-navigation";
 
 class CartPage extends Component {
+  state = {
+    loading: false,
+    products: [],
+    total: ""
+  };
+
+  componentDidMount() {
+    this.getCart();
+  }
+
+  getCart = () => {
+    this.setState({ loading: true });
+    getCart()
+      .then(res => {
+        this.setState({
+          products: res.data.data.products,
+          total: res.data.data.total,
+          loading: false
+        });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+        alert(catchError(err));
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Cart</Text>
-        <Button
-          title={"Payment Info"}
-          onPress={() => this.props.navigation.navigate("PaymentInfo")}
+        <NavigationEvents
+          onWillFocus={() => {
+            this.setState({ loading: true });
+            getCart()
+              .then(res => {
+                this.setState({
+                  products: res.data.data.products,
+                  total: res.data.data.total,
+                  loading: false
+                });
+              })
+              .catch(err => {
+                this.setState({ loading: false });
+                alert(catchError(err));
+              });
+          }}
         />
+        <View style={{ flex: 6 }}>
+          <CartList data={this.state.products} onUpdatePress={this.getCart} />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "white",
+            justifyContent: "space-between",
+            padding: 10
+          }}
+        >
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text>SUB TOTAL ({this.state.products.length} ITEM/S)</Text>
+            <Text>₱ {this.state.total}.00</Text>
+          </View>
+          <Button
+            title={"Checkout"}
+            containerStyle={{ marginHorizontal: 10 }}
+            buttonStyle={{ backgroundColor: "#f5a210", borderRadius: 10 }}
+            onPress={()=>this.props.navigation.navigate("ShippingInfo")}
+          />
+        </View>
       </View>
     );
   }
@@ -19,9 +85,9 @@ class CartPage extends Component {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#edeeef"
+    // alignItems: "center",
+    // justifyContent: "center"
   }
 };
 
