d59d611bd5c627a877f19b2b8903991475dd5039
fyrost <fmga.webbie@gmail.com>
2020-03-09 20:03:59 +0800

    [:hammer_and_wrench:] added OrderPage style and function

--- src/pages/app/order/OrdersPage.js
+++ src/pages/app/order/OrdersPage.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import { View } from "react-native";
 import OrderList from "../../../component/OrderList";
 import { getOrders, catchError } from "../../../misc/api";
+import { NavigationEvents } from "react-navigation";
+
 class OrdersPage extends Component {
   state = {
     loading: false,
     orders: []
   };
   UNSAFE_componentWillMount() {
+    this.getOrders();
+  }
+  getOrders() {
     this.setState({ loading: true });
     getOrders()
       .then(res => {
@@ -19,9 +24,12 @@ class OrdersPage extends Component {
       });
   }
   render() {
-    <View>
-      <OrderList data={this.state.orders} />
-    </View>;
+    return (
+      <View>
+        <NavigationEvents onWillFocus={() => this.getOrders()} />
+        <OrderList data={this.state.orders} />
+      </View>
+    );
   }
 }
 
