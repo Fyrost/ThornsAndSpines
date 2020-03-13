6a466ad7d6fcdfdf415acee2ad235aec001912c4
fyrost <fmga.webbie@gmail.com>
2020-03-13 15:11:24 +0800

    [:sparkles:] more page for selection

--- src/pages/app/more/MorePage.js
+++ src/pages/app/more/MorePage.js
@@ -1,16 +1,84 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Button } from "react-native-elements";
+import { View, Alert } from "react-native";
+import { Button, ListItem } from "react-native-elements";
+
+const topList = [
+  {
+    name: "FAQs",
+    icon: "question",
+    nav: "FAQ"
+  },
+  {
+    name: "Contact Us",
+    icon: "phone",
+    nav: "ContactUs"
+  },
+  {
+    name: "My Account",
+    icon: "user",
+    nav: "MyAccount"
+  }
+];
 
 class MorePage extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>More</Text>
-        <Button
-          title={"SignUp"}
-          onPress={() => this.props.navigation.navigate("SignUp")}
-        />
+      <View
+        style={{
+          backgroundColor: "#edeeef",
+          flex: 1,
+          justifyContent: "space-between"
+        }}
+      >
+        <View>
+          {topList.map((item, index) => (
+            <ListItem
+              key={index}
+              title={item.name}
+              leftIcon={{
+                type: "font-awesome",
+                name: item.icon
+              }}
+              onPress={() => {
+                this.props.navigation.navigate(item.nav);
+              }}
+              bottomDivider
+              chevron
+            />
+          ))}
+        </View>
+        <View>
+          <ListItem
+            title={"Logout"}
+            leftIcon={{
+              type: "font-awesome",
+              name: "sign-out"
+            }}
+            onPress={() => {
+              Alert.alert(
+                "",
+                "Logout?",
+                [
+                  {
+                    text: "Cancel",
+                    onPress: () => {
+                      return null;
+                    }
+                  },
+                  {
+                    text: "Logout",
+                    onPress: () => {
+                      this.props.navigation.navigate("Login");
+                    }
+                  }
+                ],
+                { cancelable: false }
+              );
+            }}
+            topDivider
+            chevron
+          />
+        </View>
       </View>
     );
   }
